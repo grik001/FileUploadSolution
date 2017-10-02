0b9b18ddaf05aa24b5d711fb294aba50639f544d
grik001 <keith.a.gri@gmail.com>
2017-10-02 19:10:08 +0200

    Added basic data helpers for File(DB)

---
+++ Data/DataModels/IDataModels/IFileDataModel.cs
@@ -0,0 +1,20 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.DataModels
+{
+    public interface IFileDataModel
+    {
+        List<File> Get();
+        File Get(Guid id);
+        List<File> Get(string userID);
+
+        File Insert(File file);
+        int Update(File file);
+        bool Delete(Guid id);
+    }
+}
