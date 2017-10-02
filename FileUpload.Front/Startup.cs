ef4618259182c4bb37ce71e0decec82368d70123
grik001 <keith.a.gri@gmail.com>
2017-10-02 17:45:48 +0200

    Add project files.

---
+++ FileUpload.Front/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FileUpload.Front.Startup))]
+namespace FileUpload.Front
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
