5a120b625a76e1a19471e770564b03c7085ebb61
grik001 <keith.a.gri@gmail.com>
2017-10-03 17:52:40 +0200

    Added Autofac to solution and injected helpers that will be used with API and Website Functions

--- FileUpload.Front/Controllers/FileController.cs
+++ FileUpload.Front/Controllers/FileController.cs
@@ -1,4 +1,5 @@
-﻿using Data.DataModels;
+﻿using Common.Helpers.IHelpers;
+using Data.DataModels;
 using Entities;
 using System;
 using System.Collections.Generic;
@@ -11,9 +12,9 @@ namespace FileUpload.Front.Controllers
     {
         private IFileDataModel _fileDataModel;
 
-        public FileController()
+        public FileController(IFileDataModel fileDataModel)
         {
-            this._fileDataModel = new FileDataModel();
+            this._fileDataModel = fileDataModel;
         }
 
         public IHttpActionResult Get()
--- FileUpload.Front/Global.asax.cs
+++ FileUpload.Front/Global.asax.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using Common.Helpers;
+using Common.Helpers.IHelpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -11,13 +16,32 @@ namespace FileUpload.Front
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static IContainer Container;
+
         protected void Application_Start()
         {
+            SetupDependancyInjection();
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        public void SetupDependancyInjection()
+        {
+            var config = GlobalConfiguration.Configuration;
+            var builder = new ContainerBuilder();
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<RabbitMQHelper>().As<IMessageQueueHelper>().SingleInstance();
+            builder.RegisterType<RedisHelper>().As<ICacheHelper>().SingleInstance();
+            builder.RegisterType<Log4NetHelper>().As<ILogger>().SingleInstance();
+            builder.RegisterType<ApplicationConfig>().As<IApplicationConfig>().SingleInstance();
+
+            Container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(Container);
         }
     }
 }
