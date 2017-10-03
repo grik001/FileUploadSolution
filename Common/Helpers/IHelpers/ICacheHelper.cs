58c84caa3f28b6ad869084b375692d8cdd7a4f49
KeithGrima <keith_grima@live.com>
2017-10-03 07:38:00 +0200

    Added basic helpers for RedisCache

---
+++ Common/Helpers/IHelpers/ICacheHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Helpers.IHelpers
+{
+    public interface ICacheHelper
+    {
+        T GetValue<T>(string key);
+
+        bool SetValue<T>(string key, T value);
+    }
+}
